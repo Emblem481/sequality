9318d05e151c35cf3eff3484da87150afc2a2dff
Yuma <209893527+Emblem481@users.noreply.github.com>
2026-07-06 10:38:22 +0900

    課題1

--- app/src/main/java/org/example/Calculate.java
+++ app/src/main/java/org/example/Calculate.java
@@ -1,7 +1,26 @@
 package org.example;
 
 public class Calculate {
-  public int sum(int x, int y){
+  public int sum(int x, int y) {
     return x + y;
+  }
+
+  public int getIndividualFee(int age) {
+    if (age <= 12) {
+      return 1000;
+    } else {
+      return 2000;
+    }
+  }
+
+  public int getTotalFee(int age, int groupSize) {
+    int individualFee = getIndividualFee(age);
+    int total = individualFee * groupSize;
+
+    if (groupSize >= 10) {
+      total = (int) (total * 0.9);
+    }
+
+    return total;
   }
 }
--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -4,10 +4,32 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CalculateTest {
+
   @Test
   public void testSum() {
-    Calculate calculate = new Calculate();
-    int expected = 5;
-    assertEquals(expected, calculate.sum(2, 3));
+    Calculate calc = new Calculate();
+    assertEquals(5, calc.sum(2, 3));
+  }
+
+  @Test
+  public void testGetIndividualFee() {
+    Calculate calc = new Calculate();
+
+    assertEquals(1000, calc.getIndividualFee(12));
+
+    assertEquals(2000, calc.getIndividualFee(13));
+  }
+
+  @Test
+  public void testGetTotalFee() {
+    Calculate calc = new Calculate();
+
+    assertEquals(1000, calc.getTotalFee(10, 1));
+
+    assertEquals(2000, calc.getTotalFee(25, 1));
+
+    assertEquals(13500, calc.getTotalFee(10, 15));
+
+    assertEquals(27000, calc.getTotalFee(25, 15));
   }
 }
